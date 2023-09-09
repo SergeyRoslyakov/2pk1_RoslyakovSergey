11ffc1c76c5043ae2493f738e1719780d3fc3641
Сергей <roslyakov20_06@mail.ru>
2023-09-09 16:45:09 +0500

    Добавьте файлы проекта.

---
+++ PZ_01/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace PZ_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 0;
+            double b = 1;
+            double c = Math.PI / 2;
+
+            double result = Math.Log(5) / Math.Sin(c) - Math.Sqrt(Math.Abs(-2.5 - Math.Pow(a, 2))) - (Math.Pow(10, 3) * a - b) / Math.Cos(b) + +Math.Pow(Math.Abs(-5 - Math.Pow(a, 2)), 1 / 3) - 2.5 * c;
+            
+            Console.WriteLine(result);
+        }
+    }
+}
