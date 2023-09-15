af5d1a632f66e1a8d70919700ca2f1aec5760d87
Сергей <roslyakov20_06@mail.ru>
2023-09-15 22:16:43 +0500

    добавлена 2 практика

---
+++ PZ_02/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace PZ_02
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            double a = double.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            double x, y, z;
+
+            if (a > Math.PI)
+            {
+                x = Math.Cos(a + 2 * c);
+            }
+            else
+            {
+                x = c + Math.Sqrt(a * c * c - 2 * (a + Math.PI));
+            }
+
+            if (x <= 0)
+            {
+                y = Math.Log(a + c - x);
+            }
+            else
+            {
+                y = Math.Sin(2 * a * x) + Math.Sin(a + x);
+            }
+
+            z = (2 * x + 3 * y) * (Math.Pow(a, 2) - Math.Pow(c, 2));
+
+            Console.WriteLine("Значение x: " + x);
+            Console.WriteLine("Значение y: " + y);
+            Console.WriteLine("Значение z: " + z);
+        }
+    }
+
+}
