64339d0e8c6523db357d4429170323a0d6a09867
Сергей <roslyakov20_06@mail.ru>
2023-09-16 20:08:14 +0500

    добавлена 3 практика

---
+++ PZ_03/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace PZ_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            double area = 0;
+            Console.WriteLine("Введите фигуру (круг, прямоугольник, треугольник):");
+            string figure = Console.ReadLine();
+
+            switch (figure)
+            {
+                case "круг":
+                    Console.WriteLine("Введите радиус круга:");
+                    double radius = double.Parse(Console.ReadLine());
+                    area = Math.PI * radius * radius;
+                    break;
+                case "прямоугольник":
+                    Console.WriteLine("Введите ширину прямоугольника:");
+                    double width = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Введите длину прямоугольника");
+                    double length = double.Parse(Console.ReadLine());
+                    area = width * length;
+                    break;
+                case "треугольник":
+                    Console.WriteLine("Введите основание треугольника:");
+                    double baseLength = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Введите высоту треугольника:");
+                    double triangleHeight = double.Parse(Console.ReadLine());
+                    area = 0.5 * baseLength * triangleHeight;
+                    break;
+                default:
+                    Console.WriteLine("Неверная фигура.");
+                    break;
+            }
+            Console.WriteLine($"Площадь {figure}а равна {area}");
+        }
+    }
+}
