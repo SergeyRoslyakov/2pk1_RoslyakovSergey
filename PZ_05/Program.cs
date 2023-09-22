b4276b6db4aeb5c79edffbb972a6b26da716303d
Сергей <roslyakov20_06@mail.ru>
2023-09-22 22:37:26 +0500

    добавлена 5 практика

---
+++ PZ_05/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace PZ_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 1;
+            double h = 0.2;
+            double y;
+
+            do
+            {
+                y = 8 * x + 4 * Math.Pow(x, 4) - 1.5 * Math.Pow(x, 3);
+                Console.WriteLine("x = {0}, y = {1}", x, y);
+                x += h;
+            } while (x <= 3);
+        }
+    }
+}
