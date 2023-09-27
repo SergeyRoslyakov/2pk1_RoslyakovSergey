b8765ea3a07bc816d0c27e35bac53617ab1114c3
Сергей <roslyakov20_06@mail.ru>
2023-09-27 16:24:32 +0500

    добавлена 6 практика

---
+++ PZ_06/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace PZ_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 10, -3, -5, 2, 5 };
+            int minIndex = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (Math.Abs(array[i]) < Math.Abs(array[minIndex]))
+                {
+                    minIndex = i;
+                }
+            }
+
+            Console.WriteLine("Минимальный по модулю элемент: " + array[minIndex]);
+            Console.WriteLine("Номер минимального по модулю элемента: " + (minIndex + 1));
+        }
+    }
+}
