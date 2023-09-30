8b0e27c9be2595a52bc227a95e1a9e3f9032270a
Сергей <roslyakov20_06@mail.ru>
2023-09-30 16:35:03 +0500

    добавлена 7 практика

---
+++ PZ_07/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace PZ_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = {
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+            { 7, 8, 9 }
+        };
+
+        int size = matrix.GetLength(0);
+        int product = 1;
+
+            for (int i = 0; i < size; i++)
+            {
+                int diagonalElement = matrix[i, i];
+                if (diagonalElement != 0)
+                {
+                    product *= diagonalElement;
+                }
+            }
+
+                Console.WriteLine("Произведение ненулевых диагональных элементов: " + product);
+
+                Console.ReadLine();
+        }
+    }
+}
