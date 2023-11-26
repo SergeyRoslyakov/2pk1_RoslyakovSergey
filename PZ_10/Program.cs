94065bff85730201e40b93abda75d9a3a39a0e92
Сергей <roslyakov20_06@mail.ru>
2023-11-26 19:15:54 +0500

    добавлена 10 практика

---
+++ PZ_10/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace PZ_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "привет, как дела? привет, как хорошо.";
+
+            // Разделение текста на предложения по знакам пунктуации
+            string[] sentences = text.Split(new char[] { '.', '!', '?' });
+
+            // Разделение каждого предложения на слова
+            string[] words1 = sentences[0].Split(' ');
+            string[] words2 = sentences[1].Split(' ');
+
+            // Создание списка для хранения повторяющихся слов
+            List<string> repeatedWords = new List<string>();
+
+            // Поиск повторяющихся слов в обоих предложениях и добавление их в список
+            foreach (string word1 in words1)
+            {
+                foreach (string word2 in words2)
+                {
+                    if (word1 == word2)
+                    {
+                        repeatedWords.Add(word1);
+                        break;
+                    }
+                }
+            }
+
+            // Вывод повторяющихся слов на консоль
+            foreach (string word in repeatedWords)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
