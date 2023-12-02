76b361748c59d753e9ab999a105a68d9a8a48ef9
Сергей <roslyakov20_06@mail.ru>
2023-12-02 15:15:37 +0500

    добавлена 12 практика

---
+++ PZ_12/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace PZ_12
+{
+    internal class Program
+    {
+        static void fullsum(double firstsum, int months)
+        {
+            double interestamount = (firstsum < 100000) ? 0.05 : 0.08;
+            double totalamount = firstsum;
+
+            for (int i = 0; i < months; i++)
+            {
+                totalamount += totalamount * interestamount;
+            }
+
+            Console.WriteLine("Сумма к концу срока: " + totalamount);
+        }
+
+        static void Main()
+        {
+            double depositamount = 100001; // Cумма вклада
+            int depositinmonths = 12; // Срок вклада в месяцах
+            fullsum(depositamount, depositinmonths);
+        }
+    }
+}
