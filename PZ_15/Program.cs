8ea063b3016b72bd2e03d72c44df7423df630a49
Сергей <roslyakov20_06@mail.ru>
2023-12-08 17:57:27 +0500

    добавлена 15 практика

---
+++ PZ_15/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace PZ_15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:\\Program Files"; // Указываем путь
+            if (Directory.Exists(path)) // Проверяем существование каталога
+            {
+                var directories = new DirectoryInfo(path).GetDirectories();
+                Array.Sort(directories, (x, y) => y.Name.Length.CompareTo(x.Name.Length));
+
+                foreach (var directory in directories)
+                {
+                    Console.WriteLine(directory.Name); // Выводим отсортированные каталоги в консоль
+                }
+            }
+            else
+            {
+                Console.WriteLine("Каталог не существует");
+            }
+        }
+    }
+}
